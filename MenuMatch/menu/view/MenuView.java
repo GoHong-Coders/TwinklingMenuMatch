34faa8353b388cc283673fda745ddc6f04d237fc
kye <kye6735@naver.com>
2023-07-30 06:20:41 +0900

    feat : 버튼 위치 수정 및 버튼 바탕 이미지 추가

--- MenuMatch/menu/view/MenuView.java
+++ MenuMatch/menu/view/MenuView.java
@@ -2,17 +2,12 @@ package menu.view;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.*;
+import java.awt.event.*;
 
 public class MenuView extends JFrame{
 
     // 패널
     JPanel tapPn = new JPanel(null){
-//        @Override
-//        public void setBounds(int x, int y, int width, int height) {
-//            super.setBounds(0, 0, 1200, 120);
-//        }
-
         @Override
         protected void paintComponent(Graphics g){
             super.paintComponent(g);
@@ -23,13 +18,7 @@ public class MenuView extends JFrame{
             g.drawImage(image, 0, 0, this);
         }
     };
-    JPanel menuPn = new JPanel(null){
-//        @Override
-//        public void setBounds(int x, int y, int width, int height) {
-//            super.setBounds(24, 197, 1140, 803);
-//        }
-
-    };
+    JPanel menuPn = new JPanel(null);
 
     // 버튼
     JButton homeBtn = new JButton("");
@@ -50,6 +39,7 @@ public class MenuView extends JFrame{
     ImageIcon morningBtnClick_img = new ImageIcon("imgs/morning.png");
     ImageIcon lunchBtnClick_img = new ImageIcon("imgs/lunch.png");
     ImageIcon dinnerBtnClick_img = new ImageIcon("imgs/dinner.png");
+    ImageIcon menu_img = new ImageIcon("imgs/menu.png");
 
     JScrollPane scrollPane = new JScrollPane(menuPn);
 
@@ -57,7 +47,6 @@ public class MenuView extends JFrame{
         setTitle("Menu"); // JFrame의 제목
         // setLayout(new GridLayout(2,1)); // 프레임 하나에 동시에 두 패널 띄움
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-
         setBounds(0, 0, 1200, 700); //x, y 좌표 위치 / 너비 높이 길이 설정
         setLocationRelativeTo(null); // Frame의 위치를 화면 중앙으로 설정
         setResizable(false); // 화면 크기 고정하는 작업
@@ -82,11 +71,11 @@ public class MenuView extends JFrame{
         scrollPane.setPreferredSize(new Dimension(500, 500)); // 스크롤 뷰포트의 크기 설정
 
         // 패널 크기 설정
-        int tapPn_heigth = 120;
-        int menuPn_height = tapPn_heigth * 5;
+        int tapPn_height = 120;
+        int menuPn_height = tapPn_height * 8;
 
-        tapPn.setPreferredSize(new Dimension(500, tapPn_heigth));
-        menuPn.setPreferredSize(new Dimension(500, menuPn_height));
+        tapPn.setPreferredSize(new Dimension(1200, tapPn_height));
+        menuPn.setPreferredSize(new Dimension(1100, menuPn_height));
 
         // 패널 추가
         add(tapPn);
@@ -98,15 +87,18 @@ public class MenuView extends JFrame{
     public void createButtons() { // 4행 4열 배열 버튼 생성
         for (int row = 0; row < 4; row++) {
             for (int col = 0; col < 4; col++) {
-                buttons[row][col] = new JButton("" + (row * 4 + col + 1));
+                buttons[row][col] = new JButton("" );
                 buttons[row][col].setPreferredSize(new Dimension(100, 50)); // 버튼 크기 설정 (가로 100, 세로 50)
 
+                buttons[row][col].setIcon(menu_img);
+                buttons[row][col].setBorderPainted(false);
+                buttons[row][col].setContentAreaFilled(false);
             }
         }
     }
 
     public void addButtonsToPanel() { // 4행 4열 버튼 패널에 추가
-        menuPn.setLayout(new GridLayout(4, 10, 58, 82)); // 10픽셀의 간격으로 설정
+        menuPn.setLayout(new GridLayout(4, 10, -26, 0)); // 10픽셀의 간격으로 설정
         for (int row = 0; row < 4; row++) {
             for (int col = 0; col < 4; col++) {
                 menuPn.add(buttons[row][col]);
