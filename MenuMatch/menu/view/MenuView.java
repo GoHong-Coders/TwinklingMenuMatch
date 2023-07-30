337ed83b7935d8d05cbc040b8269a015ceacf207
kye <kye6735@naver.com>
2023-07-30 20:56:15 +0900

    feat : 메뉴 설명창 UI 및 기능 구현

--- MenuMatch/menu/view/MenuView.java
+++ MenuMatch/menu/view/MenuView.java
@@ -6,8 +6,11 @@ import java.awt.event.*;
 
 public class MenuView extends JFrame{
 
+    // 프레임
+    JFrame menuF = new JFrame("Menu Description"); // 메뉴 설명
+
     // 패널
-    JPanel tapPn = new JPanel(null){
+    JPanel tapPn = new JPanel(null){ // 탭바 패널
         @Override
         protected void paintComponent(Graphics g){
             super.paintComponent(g);
@@ -18,7 +21,29 @@ public class MenuView extends JFrame{
             g.drawImage(image, 0, 0, this);
         }
     };
-    JPanel menuPn = new JPanel(null);
+    JPanel menuPn = new JPanel(null); // 메뉴 버튼 패널
+    JPanel menu_descriptionPn = new JPanel(null){ // 메뉴 설명 패널
+        @Override
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+            ImageIcon imageIcon = new ImageIcon("imgs/Menu_Description.png");
+            Image image = imageIcon.getImage();
+
+            // 이미지를 패널에 그림
+            g.drawImage(image, 0, 0, this);
+        }
+    };
+    JPanel menu_WindowPn = new JPanel(null){ // 메뉴 설명창
+        @Override
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+            ImageIcon imageIcon = new ImageIcon("imgs/white.png");
+            Image image = imageIcon.getImage();
+
+            // 이미지를 패널에 그림
+            g.drawImage(image, 0, 0, this);
+        }
+    };
 
     // 버튼
     JButton homeBtn = new JButton("");
@@ -27,6 +52,9 @@ public class MenuView extends JFrame{
     JButton morningBtn = new JButton("");
     JButton lunchBtn = new JButton("");
     JButton dinnerBtn = new JButton("");
+    JButton menuXBtn = new JButton("");
+    JButton menuCloseBtn = new JButton("");
+    JButton menuSelectBtn = new JButton("");
     JButton[][] buttons = new JButton[4][4]; // 4행 4열의 버튼 배열 선언
 
     // 이미지 삽입
@@ -39,6 +67,9 @@ public class MenuView extends JFrame{
     ImageIcon morningBtnClick_img = new ImageIcon("imgs/morning.png");
     ImageIcon lunchBtnClick_img = new ImageIcon("imgs/lunch.png");
     ImageIcon dinnerBtnClick_img = new ImageIcon("imgs/dinner.png");
+    ImageIcon menuXBtn_img = new ImageIcon("imgs/x.png");
+    ImageIcon menuCloseBtn_img = new ImageIcon("imgs/close.png");
+    ImageIcon menuSelectBtn_img = new ImageIcon("imgs/select.png");
     ImageIcon menu_img = new ImageIcon("imgs/menu.png");
 
     JScrollPane scrollPane = new JScrollPane(menuPn);
@@ -62,6 +93,11 @@ public class MenuView extends JFrame{
         MorningBtn();
         LunchBtn();
         DinnerBtn();
+
+        // 메뉴 설명 버튼 추가
+        MenuCloseBtn();
+        MenuXBtn();
+        MenuSelectBtn();
 
         // 버튼 배열 추가
         addButtonsToPanel();
@@ -98,9 +134,29 @@ public class MenuView extends JFrame{
     }
 
     public void addButtonsToPanel() { // 4행 4열 버튼 패널에 추가
-        menuPn.setLayout(new GridLayout(4, 10, -26, 0)); // 10픽셀의 간격으로 설정
-        for (int row = 0; row < 4; row++) {
-            for (int col = 0; col < 4; col++) {
+        menuPn.setLayout(new GridLayout(4, 4, -26, 0)); // 10픽셀의 간격으로 설정
+        int row, col;
+        for (row = 0; row < 4; row++) {
+            for (col = 0; col < 4; col++) {
+                buttons[row][col].addMouseListener(new MouseAdapter() { // 마우스 이벤트
+                    @Override public void mousePressed(MouseEvent e) { // 마우스 클릭했을때
+                        menuF.setVisible(true);
+                        menuF.setSize(670, 680);
+                        menuF.setLayout(null);
+                        menuF.setLocationRelativeTo(null);
+                        menuF.setResizable(false);
+                        menuF.add(menu_descriptionPn);
+                        menu_descriptionPn.add(menu_WindowPn);
+                        menu_descriptionPn.setBounds(0,0,670,680);
+                        menu_WindowPn.setBounds(50, 80, 560, 400);
+
+                        menuF.addWindowListener(new WindowAdapter() {
+                            @Override public void windowClosing(WindowEvent e) { // X 누를 시 메뉴 설명 화면이 사라짐
+                                menuF.dispose();
+                            }
+                        });
+                    }
+                });
                 menuPn.add(buttons[row][col]);
             }
         }
@@ -155,6 +211,42 @@ public class MenuView extends JFrame{
         dinnerBtn.setIcon(dinnerBtn_img);
         dinnerBtn.setContentAreaFilled(false);
         tapPn.add(dinnerBtn);
+    }
+
+    public void MenuXBtn(){ // 메뉴 설명 X 버튼 (닫기 버튼과 같음)
+        menuXBtn.setBounds(580, 20, 37, 37);
+        menuXBtn.setBorderPainted(false);
+        menuXBtn.setIcon(menuXBtn_img);
+        menuXBtn.setContentAreaFilled(false);
+        menu_descriptionPn.add(menuXBtn);
+
+        menuXBtn.addMouseListener(new MouseAdapter() {
+            @Override public void mousePressed(MouseEvent e) {
+                menuF.dispose();
+            }
+        });
+    }
+
+    public void MenuCloseBtn(){ // 메뉴 설명 닫기 버튼
+        menuCloseBtn.setBounds(140, 520, 151, 69);
+        menuCloseBtn.setBorderPainted(false);
+        menuCloseBtn.setIcon(menuCloseBtn_img);
+        menuCloseBtn.setContentAreaFilled(false);
+        menu_descriptionPn.add(menuCloseBtn);
+
+        menuCloseBtn.addMouseListener(new MouseAdapter() {
+            @Override public void mousePressed(MouseEvent e) {
+                menuF.dispose();
+            }
+        });
+    }
+
+    public void MenuSelectBtn(){ // 메뉴 선택 버튼
+        menuSelectBtn.setBounds(373, 520, 151, 69);
+        menuSelectBtn.setBorderPainted(false);
+        menuSelectBtn.setIcon(menuSelectBtn_img);
+        menuSelectBtn.setContentAreaFilled(false);
+        menu_descriptionPn.add(menuSelectBtn);
     }
 
     public static void main(String[] args) {
