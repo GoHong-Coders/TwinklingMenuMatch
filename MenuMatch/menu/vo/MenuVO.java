afd3e408fa0ad592c32a44dc9f7182455bfc1dd8
mic050r <103114387+mic050r@users.noreply.github.com>
2023-08-17 01:43:39 +0900

    feat : 유정 관련 정보, 선택 메뉴 정보 필드 생성

--- MenuMatch/menu/vo/MenuVO.java
+++ MenuMatch/menu/vo/MenuVO.java
@@ -1,4 +1,16 @@
 package menu.vo;
+import java.util.ArrayList;
+
 
 public class MenuVO {
+    ArrayList<MenuVO> svoList = new ArrayList<MenuVO>();
+
+    //insert() : 메뉴 정보 객체 참조값을 ArrayList에 추가
+    public void insert(MenuVO svo) {
+        svoList.add(svo);
+    }
+    //select() : 메뉴 정보들이 저장된 ArrayList객체 참조값을 반환
+    public ArrayList<MenuVO> select(){
+        return svoList;
+    }
 }
