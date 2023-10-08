92ad94c12d04bb6ef3fb57733bb718b486d5253b
mic050r <103114387+mic050r@users.noreply.github.com>
2023-10-08 21:53:21 +0900

    feat : 아침 메뉴 설명, 이름 불러오기 및 선택하면 기록 db에 추가

---
+++ MenuMatch/menu/dto/BreakFastDTO.java
@@ -0,0 +1,50 @@
+package menu.dto;
+
+public class BreakFastDTO {
+    private int MenuID;
+    private String MenuName;
+    private String Description;
+
+    public BreakFastDTO(int menuID, String menuName, String description) {
+        MenuID = menuID;
+        MenuName = menuName;
+        Description = description;
+    }
+
+    public BreakFastDTO() {
+
+    }
+
+    public int getMenuID() {
+        return MenuID;
+    }
+
+    public void setMenuID(int menuID) {
+        MenuID = menuID;
+    }
+
+    public String getMenuName() {
+        return MenuName;
+    }
+
+    public void setMenuName(String menuName) {
+        MenuName = menuName;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "DinnerDTO{" +
+                "MenuID=" + MenuID +
+                ", MenuName='" + MenuName + '\'' +
+                ", Description='" + Description + '\'' +
+                '}';
+    }
+}
